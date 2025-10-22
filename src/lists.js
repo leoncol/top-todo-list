14c8af8a3a787f992038652a5ea796d933c0998a
leoncol <camiloleon196@gmail.com>
2025-10-22 15:41:26 -0500

    Create form for creating tasks

--- src/lists.js
+++ src/lists.js
@@ -1,7 +1,10 @@
 import "./styles.css";
 import caretActivation from "./caret.js";
+import todayDate from "./lists-dates.js"
 import { updateSelectedList } from "./dom-update-selected-list.js";
 
 updateSelectedList();
+
+
 
 console.log('Hello world!');
